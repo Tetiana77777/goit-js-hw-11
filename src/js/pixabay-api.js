d440ae717b6bab43ec0030792694f1ed1353d3ff
Tetiana77777 <147824605+Tetiana77777@users.noreply.github.com>
2024-03-27 18:15:30 +0300

    add 4

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,5 @@
 export function getImage(inputValue) {
-  const BASE_URL = 'https://pixabay.com';
-  const END_POINT = '/api';
+  const BASE_URL = 'https://pixabay.com/api';
   const params = new URLSearchParams({
     key: '43094925-102acc99687b818cc3e092daf',
     q: inputValue,
@@ -8,7 +7,7 @@ export function getImage(inputValue) {
     orientation: 'horizontal',
     safesearch: true,
   });
-  const url = `${BASE_URL}${END_POINT}?${params}`;
+  const url = `${BASE_URL}?${params}`;
 
   return fetch(url).then(response => {
     if (!response.ok) {
